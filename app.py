dea3acbec3deeced9b3305eb00b9a48951e2b287
Ozan Şelte <ozanselte@gmail.com>
2022-05-30 20:41:48 +0300

    fixed json

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 from flask import Flask, request
 import os
-import bson
+import json
 
 MONGODB_URL = 'mongodb+srv://redstone_app:huseyinnodejs@cluster0.k4qd1.mongodb.net/?retryWrites=true&w=majority'
 app = Flask(__name__)
@@ -16,9 +16,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 @app.route('/sendvalue', methods=['POST', 'GET'])
 def results():
     print(request.data)
-    decoded_doc = bson.BSON(request.data).decode()
-    print(decoded_doc)
-    collection.insert_one(decoded_doc)
+    req_dict = json.loads(request.data)
+    collection.insert_one(req_dict)
     return ''
 
 
