75826d7347c7da4a3ac23f5215b4ba993d53ce71
Ozan Şelte <ozanselte@gmail.com>
2022-06-03 10:26:06 +0300

    added error print

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def results():
             for i in range(4):
                 req_dict['raw'][i].append(nums[i])
         except:
+            print(err)
             pass
     if 'result' in req_dict.keys():
         return 'Hiphop'
