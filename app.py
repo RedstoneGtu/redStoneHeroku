a085241e7b349edc5db8989b9c36c7366971e44b
Ozan Şelte <ozanselte@gmail.com>
2022-06-03 11:54:24 +0300

    Added model

--- app.py
+++ app.py
@@ -4,13 +4,52 @@ from flask import Flask, request
 import os
 import json
 import base64
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+NAMES = {
+    0: 'Classic',
+    1: 'Electro',
+    2: 'Jazz',
+    3: 'Rock',
+    4: 'Pop',
+    5: 'Hiphop',
+}
 
 MONGODB_URL = 'mongodb+srv://redstone_app:huseyinnodejs@cluster0.k4qd1.mongodb.net/?retryWrites=true&w=majority'
 app = Flask(__name__)
 client = MongoClient(MONGODB_URL, server_api=ServerApi('1'))
 db = client['SubjectsDB']
 collection = db['subjects']
+pickle_mo = pickle.load(open('model.pkl','rb'))
 
+def predict_fun(col):
+    a = []
+    last = [col]
+    df_last = pd.DataFrame(last)
+    raw_ = df_last['raw']
+    last = []
+    last.append([])
+    last = list((raw_))
+    a = raw_.to_numpy().flatten()
+    b = [[] for _ in range(len(raw_))]
+    for i in range(len(raw_)):
+        for j in range(len(a[i][0])):
+            b[i].append(a[i][0][j])
+            b[i].append(a[i][1][j])
+    for i in range(len(b)):
+        b[i] = np.split(np.array(b[i]),6)
+    b = np.array(b)
+    x = []
+    for i in range(len(b)): 
+        x.append(b[i].mean(axis=1))
+    print(np.array(b[0]).shape)
+    last = pd.DataFrame(x)
+    pred = pickle_mo.predict(last)
+    return pred
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -29,7 +68,8 @@ def results():
             print('err')
             pass
     if 'result' not in req_dict.keys():
-        return 'Hiphop'
+        prediction = predict_fun(req_dict)
+        return NAMES[prediction]
     else:
         collection.insert_one(req_dict)
         return 'Added to the database'
