d2c4be2fe08b94b5f2ce810da8eac5f374c4b0f8
Ozan Şelte <ozanselte@gmail.com>
2022-06-03 13:56:32 +0300

    Added import

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import pickle
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
 NAMES = {
